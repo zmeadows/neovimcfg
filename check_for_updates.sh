e2b605129031caa10181e7dc7d42a896d7dc3a75
Zachary Alden Meadows <zachary.alden.meadows@cern.ch>
2019-09-30 12:51:27 -0400

    using flock to avoid issues when opening lots of terminals at once

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+set -e
+
 OLDDIR=$(pwd)
 cd $NEOVIM_CFG_REPO
+
+# create a lock file, to prevent git errors when attempting to run
+# this script from many terminals at once (ex: open 10 terminals at same time)
+LOCK="/tmp/zmeadows_neovimcfg_update_lock"
+exec 200>$LOCK
+flock -n 200 || exit 1
 
 if [[ $(git status -s) ]]; then
     echo "You have un-commited changes to neovim cfg, fix it!"
